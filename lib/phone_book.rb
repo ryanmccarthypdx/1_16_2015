e0aa757d4f633c7b860c4b52bfe66a57afa4bb1f
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-01-16 09:46:14 -0800

    Add #save and .all methods

---
+++ lib/phone_book.rb
@@ -0,0 +1,28 @@
+class Contact
+  attr_reader(:name, :info)
+
+  @@all_contacts = []
+
+  define_method(:initialize) do |attribute|
+    @name = attribute.fetch(:name)
+    @info = attribute.fetch(:info)
+  end
+
+  define_method(:save) do
+    @@all_contacts.push(self)
+    @id = @@all_contacts.length()
+    self
+  end
+
+  define_singleton_method(:all) do
+    @@all_contacts
+  end
+
+  
+
+
+
+
+
+
+end
--- spec/phone_book_spec.rb
+++ spec/phone_book_spec.rb
@@ -1,3 +1,25 @@
 require('rspec')
 require('phone_book')
 require('pry')
+
+describe(Contact) do
+
+  describe('#save') do
+    it("saves an entry to the main list") do
+      test_contact = Contact.new({ :name => "Mr Plow", :info => "503-KL5-3223" })
+      test_contact.save()
+      expect(Contact.all()).to(eq([test_contact]))
+    end
+
+  end
+
+
+
+
+
+
+
+
+
+
+end
